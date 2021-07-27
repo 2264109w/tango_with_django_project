acb98f6362bf1bf252aa88217df6871eb8c02bd0
Grant Woollon <2264109w@student.gla.ac.uk>
2021-07-27 15:46:26 +0100

    chapter 10 done

--- rango/views.py
+++ rango/views.py
@@ -42,19 +42,20 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
-    response = render(request, 'rango/index.html', context=context_dict)
-    return response
-
+    return render(request, 'rango/index.html', context=context_dict)
+    
 def about(request):
     # prints out whether the method is a GET or a POST
     print(request.method)
     # prints out the user name, if no one is logged in it prints `AnonymousUser`
     print(request.user)
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
 
 
     
-    return render(request, 'rango/about.html', {})
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
@@ -261,8 +262,8 @@ def visitor_cookie_handler(request):
     # We use the COOKIES.get() function to obtain the visits cookie.
     # If the cookie exists, the value returned is casted to an integer.
     # If the cookie doesn't exist, then the default value of 1 is used.
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7],  '%Y-%m-%d %H:%M:%S')
     
     # If it's been more than a day since the last visit...
